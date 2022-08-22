256f2fb22f4d696cc2d2f7adf0a204cdf9fb535f
David BREYAULT <davidbreyault@gmail.com>
2022-08-22 15:56:55 +0200

    [feature-01] Ajout d'un AppUser en base

--- src/main/java/com/tp/birthdayapp/model/AppUser.java
+++ src/main/java/com/tp/birthdayapp/model/AppUser.java
@@ -2,23 +2,21 @@ package com.tp.birthdayapp.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "user", schema = "public")
 public class AppUser {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
     private String email;
 
-    public AppUser(Long id, String username, String password, String email) {
-        this.id = id;
+    public AppUser(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
