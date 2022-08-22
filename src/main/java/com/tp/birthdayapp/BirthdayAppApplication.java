0d8cef1a86eb20fbc3757d304096e20d5b7d860c
Max-Diard <themax41@hotmail.fr>
2022-08-22 16:55:33 +0200

    Suppression de la ligne du repo

--- src/main/java/com/tp/birthdayapp/BirthdayAppApplication.java
+++ src/main/java/com/tp/birthdayapp/BirthdayAppApplication.java
@@ -3,6 +3,8 @@ package com.tp.birthdayapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class BirthdayAppApplication {
 
--- src/main/java/com/tp/birthdayapp/repository/BirthdayRepository.java
+++ src/main/java/com/tp/birthdayapp/repository/BirthdayRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BirthdayRepository extends JpaRepository<Birthday, Long> {
-    boolean exists(Birthday birthday);
     List<Birthday> findBirthdayByAppUser(AppUser appUser);
 }
